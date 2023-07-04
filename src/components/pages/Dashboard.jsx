74cf1cf0eb38bc27afac0ca4fcd97af45eedf767
iamshiv007 <iamshiv20032003@gmail.com>
2023-07-05 00:40:23 +0530

    Dashboard created

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import "../../styles/Dashboard.css";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="dashboard">
+      <h1 style={{ marginBottom: "12px", color: "white" }}>Dashboard</h1>
+      <div className="summary">
+        <div className="card">
+          <h4>App Users</h4>
+          <p>
+            19000 <span className="negative">-18%</span>
+          </p>
+        </div>
+        <div className="card">
+          <h4>Genius Coin Rate</h4>
+          <p>
+            $10 <span className="positive">+4%</span>{" "}
+          </p>
+        </div>
+        <div className="card">
+          <h4>Total Sell Coin</h4>
+          <p>
+            5000 <span className="negative">-7%</span>
+          </p>
+        </div>
+        <div className="card">
+          <h4>Total Registered Users</h4>
+          <p>
+            5000 <span className="positive">+12%</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
